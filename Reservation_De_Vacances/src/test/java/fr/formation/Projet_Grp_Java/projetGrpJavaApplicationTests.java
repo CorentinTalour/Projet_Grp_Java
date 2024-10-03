a8dea6981916b98d8b8ce94385ddacbc1c5f5058
@kilianpfr <kilian3644@gmail.com>
2024-10-03 10:04:29 +0200

    Mise au propre des noms

--- Reservation_De_Vacances/src/test/java/fr/formation/Projet_Grp_Java/projetGrpJavaApplicationTests.java
+++ Reservation_De_Vacances/src/test/java/fr/formation/Projet_Grp_Java/projetGrpJavaApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package fr.formation.Projet_Grp_Java;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class ProjetGrpJavaApplicationTest {
 
     @Test
     void contextLoads() {
