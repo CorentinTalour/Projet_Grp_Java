3e679ddc68f604499b7a19b4784e09e8a9713d10
Florent <florent.guyard@live.fr>
2024-10-20 21:45:37 +0200

    ajout model

---
+++ Reservation_De_Vacances/src/main/java/fr/formation/Projet_Grp_Java/model/Booking.java
@@ -0,0 +1,37 @@
+package fr.formation.Projet_Grp_Java.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.UuidGenerator;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@Entity
+@Table(name = "booking")
+@Getter
+@Setter
+public class Booking {
+
+    @Id
+    @UuidGenerator
+    @Column(name = "id")
+    private String id;
+
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    @Column(name = "booking_begin", nullable = false)
+    private LocalDateTime dateBegin;
+
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    @Column(name = "booking_end", nullable = false)
+    private LocalDateTime dateEnd;
+
+    private float price;
+
+}
